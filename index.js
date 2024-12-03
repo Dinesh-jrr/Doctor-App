23baa8b4c4487e663d96ea05de6b37c005642f8a
DineshJrr <jrrdinesh2107@gmail.com>
2024-12-03 21:29:24 +0545

    backend setup

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const app=express()
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+
+app.get('/',async (req,res)=>{
+    res.status(500).send("Hello User");
+});
+
+const port=8800
+app.listen(port,()=>console.log("Server is running in port:",port))
+
+
